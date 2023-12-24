bb06e6d92a7effeacedb59b9f9cdee638f8edfc1
mauro <m.a.esquenon@hotmail.com>
2023-12-24 09:11:18 -0300

    small fixes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
-// import { useNDK } from '@nostr-dev-kit/ndk-react'
+import { Button, Container, Row, Col } from 'react-bootstrap'
+import { useNDK } from '@nostr-dev-kit/ndk-react'
 import { useLoginNIP07 } from 'hooks/LoginNIP07'
+import Image from 'react-bootstrap/Image'
 
 export default function LogInExtension() {
   const { result, loading, connectExtension } = useLoginNIP07()
+  const { getProfile } = useNDK()
+
+  const profile = result?.user?.npub && getProfile(result?.user?.npub)
+  console.log('profile', profile)
 
   return (
-    <>
-      <Button onClick={() => connectExtension()} disabled={loading}>
-        {loading ? '...' : 'Login'}
-      </Button>
-
-      {result && (
-        <pre>
-          <code>{result}</code>
-        </pre>
-      )}
-    </>
+    <Container>
+      <Row>
+        <Button onClick={() => connectExtension()} disabled={loading}>
+          {loading ? '...' : profile?.displayName ? profile?.displayName : 'Login'}
+        </Button>
+      </Row>
+      <Row>
+        <Col>
+          <Image src={profile?.image} alt={profile?.displayName} thumbnail />
+        </Col>
+      </Row>
+    </Container>
   )
 }
--- src/hooks/LoginNIP07.js
+++ src/hooks/LoginNIP07.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useNDK } from '@nostr-dev-kit/ndk-react'
 
 export const useLoginNIP07 = () => {
-  const [result, setResult] = useState('')
+  const [result, setResult] = useState({})
   const [loading, setLoading] = useState(false)
 
   const { loginWithNip07 } = useNDK()
@@ -11,7 +11,7 @@ export const useLoginNIP07 = () => {
     setLoading(true)
     const user = await loginWithNip07()
     if (user) {
-      setResult(JSON.stringify(user, null, 2))
+      setResult(user)
     }
     setLoading(false)
   }
